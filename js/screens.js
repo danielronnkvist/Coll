855f62436e5e2c0028d7af299a8fc8e18b2b6dd1
alundin <alilu684@student.liu.se>
2014-09-07 10:19:07 +0200

    implemented doughnut chart

--- js/screens.js
+++ js/screens.js
@@ -5,7 +5,7 @@ var dayView = $("#main-1"),
 
 current = "dayView"
 
-target.html(dayView.html());
+target.html(main3.html());
 target.swipe( {fingers: 'all', swipeRight: rightSwipe, swipeLeft: leftSwipe, allowPageScroll:'auto'} );
 
 function rightSwipe(){
@@ -28,6 +28,8 @@ function rightSwipe(){
       }, 200, function(){
         target.html(main2.html())
         target.css("margin-left", "0");
+        // ADD FUNCTION HERE
+        start();
       })
       current = "main2"
     break;
@@ -56,6 +58,8 @@ function leftSwipe(){
       }, 200, function(){
         target.html(main3.html())
         target.css("margin-left", "0");
+        // ADD FUNCTION HERE
+        start();
       })
       current = "main3"
     break;
