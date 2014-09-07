441df54c9de7778c4a2b166c64f4544c9f961f7f
danielronnkvist <daniel@ronnkvist.com>
2014-09-07 11:44:06 +0200

    added files

---
+++ js/variable.js
@@ -0,0 +1,39 @@
+var trips = [
+  {
+    "type": "Car",
+    "classification": 3,
+    "start": 0,
+    "end": 1.3,
+    "affect": 0.1
+  },{
+    "type": "Walking",
+    "classification": 1,
+    "start": 1.8,
+    "end": 2.2,
+    "affect": 0.04
+  },{
+    "type": "Car",
+    "classification": 3,
+    "start": 2.2,
+    "end": 4,
+    "affect": 0.16
+  },{
+    "type": "Bus",
+    "classification": 2,
+    "start": 5.3,
+    "end": 5.8,
+    "affect": 0.04
+  },{
+    "type": "Car",
+    "classification": 3,
+    "start": 9,
+    "end": 10,
+    "affect": 0.25
+  },{
+    "type": "Walking",
+    "classification": 1,
+    "start": 10,
+    "end": 12,
+    "affect": 0.04
+  },
+];
